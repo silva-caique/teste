f13edea161278fc76fc9d5dfcd77c6c355d2dc39
Caique Silva <silvacaiquee1@gmail.com>
2026-09-24 21:38:43 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,19 @@
 self.addEventListener('install', (event) => {
-    console.log('App pronta a ser instalada!');
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request));
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+// Escuta a mensagem enviada do index.html para disparar a notificação
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'NOTIFICACAO') {
+        self.registration.showNotification(event.data.titulo, {
+            body: event.data.corpo,
+            icon: 'https://cdn-icons-png.flaticon.com/512/3074/3074058.png',
+            badge: 'https://cdn-icons-png.flaticon.com/512/3074/3074058.png',
+            vibrate: [200, 100, 200]
+        });
+    }
 });
